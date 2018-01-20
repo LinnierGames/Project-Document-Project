85888501dc185f61a6753ccac3ab7e1667378b06
LinnierGames <esericksanc@gmail.com>
2018-01-19 19:37:39 -0800

    init PhotoCollectionViewController

---
+++ Project-Document-Project/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  Project-Document-Project
+//
+//  Created by Erick Sanchez on 1/19/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageview: UIImageView!
+    
+}
